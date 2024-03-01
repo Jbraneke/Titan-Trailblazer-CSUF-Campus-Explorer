ed0495001fd7c1980cfc968dac4d01306466e22a
defianttz <80181373+defianttz@users.noreply.github.com>
2024-03-01 15:59:58 -0800

    Added required map packages/API. Include a simple example of marking the map.

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapExample from './src/example/MapExample';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Titan Explorer is coming soon!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> 
+      <MapExample />
     </View>
   );
 }
